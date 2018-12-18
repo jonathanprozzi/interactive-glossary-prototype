8429b5dbab90cd37c3e1bbf3c97b6a728fe14863
jonathanprozzi <jonathanprozzi@gmail.com>
2018-12-18 12:49:23 -0500

    removed the console.log from the toggle function

--- src/data.js
+++ src/data.js
@@ -2,20 +2,20 @@ export const terms = [
   {
     id: 1,
     title: "Prototype",
-    question: "",
+    question: "What is a prototype?",
     definition: "A prototype is a first model or version of something",
     img: "../src/mini-maker-prototype.jpg"
   },
   {
     id: 2,
     title: "Term Two",
-    question: "",
+    question: "What is meant by Term Two?",
     definition: "Term Two is the second item in this list"
   },
   {
     id: 3,
     title: "Term Three",
-    question: "",
+    question: "How do we use Term Three?",
     definition: "Term Three is the third item in this list"
   },
   {
