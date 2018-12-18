3bae091ac22e926da62b996d06ebe7d8f3ff532e
jonathanprozzi <jonathanprozzi@gmail.com>
2018-12-18 14:26:39 -0500

    updated favicon with less blurry .ico

--- src/components/GlossaryCard.js
+++ src/components/GlossaryCard.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-class GlossaryCard extends React.Component {
+class GlossaryCard extends Component {
   state = {
     askingQuestion: true
   };
