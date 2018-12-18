266f28628018c378652b50956220975630ab34a0
jonathanprozzi <jonathanprozzi@gmail.com>
2018-12-18 12:39:06 -0500

    updated data.js data structure to meet new needs

--- src/data.js
+++ src/data.js
@@ -2,18 +2,21 @@ export const terms = [
   {
     id: 1,
     title: "Prototype",
-    definition: "A prototype is a first model or version of something",
+    question: "",
+    answer: "A prototype is a first model or version of something",
     img: "../src/mini-maker-prototype.jpg"
   },
   {
     id: 2,
     title: "Term Two",
-    definition: "Term Two is the second item in this list"
+    question: "",
+    answer: "Term Two is the second item in this list"
   },
   {
     id: 3,
     title: "Term Three",
-    definition: "Term Three is the third item in this list"
+    question: "",
+    answer: "Term Three is the third item in this list"
   },
   {
     id: 4
